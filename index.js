8d585d8f64bf64b63f64c256f5f8f49c3b5b9210
Amirhossein <ahmoznebi@gmail.com>
2022-09-10 21:22:24 +0430

    Variables Set

---
+++ index.js
@@ -0,0 +1,35 @@
+let canvas = document.getElementById("canvas") ; 
+
+let canvasContext = canvas.getContext("2d") ; 
+
+let createRect = (x,y,width,height,color) =>{
+    canvasContext.fillStyle = color 
+    canvasContext.fillrext(x,y,width,height)
+}
+
+let allRains = []
+
+let defaultRainWidth = 2
+let defaultRainHeight = 15 
+
+let maximumRainCount = 500
+
+let fps = 60
+
+let gameLoop = ()=>{
+    setInterval(show , 1000/fps) ;
+}
+
+let show = ()=> {
+    update()
+    draw()
+}
+
+let update = ()=> {
+
+}
+
+let draw = ()=>{
+
+}
+gameLoop()
